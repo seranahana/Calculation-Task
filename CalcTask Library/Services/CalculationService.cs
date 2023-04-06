e37168167386e128669bdc5ac1dcc7a697c63f0e
Serana <lanayamann@gmail.com>
2023-04-06 21:10:01 +0300

    Result NaN and finitation checks added

--- CalcTask Library/Services/CalculationService.cs
+++ CalcTask Library/Services/CalculationService.cs
@@ -1,29 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CalcTask.WebAPI.Library.Services
+﻿namespace CalcTask.WebAPI.Library.Services
 {
     public class CalculationService : ICalculationService
     {
         public double Add(double value, double addend)
         {
             double s = value + addend;
+            if (double.IsInfinity(s) || double.IsNaN(s))
+                throw new OverflowException();
             return s;
         }
 
         public double Divide(double value, double divisor)
         {
             double s = value / divisor;
+            if (double.IsInfinity(s) || double.IsNaN(s))
+                throw new OverflowException();
             return s;
         }
 
         public double Multiply(double value, double multiplier)
         {
             double s = value * multiplier;
+            if (double.IsInfinity(s) || double.IsNaN(s))
+                throw new OverflowException();
             return s;
         }
 
@@ -36,12 +35,16 @@ namespace CalcTask.WebAPI.Library.Services
         public double Pow(double value, double power)
         {
             double s = Math.Pow(value, power);
+            if (double.IsInfinity(s) || double.IsNaN(s))
+                throw new OverflowException();
             return s;
         }
 
         public double Substract(double value, double substrahend)
         {
             double s = value - substrahend;
+            if (double.IsInfinity(s) || double.IsNaN(s))
+                throw new OverflowException();
             return s;
         }
     }
